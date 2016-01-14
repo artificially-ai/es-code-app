746fde3163d70573d2b49ddf7ee05c3510adb66c
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2016-01-14 20:07:39 +0100

    Refactors the SelectFileAction class in order to use it with Lambda functions

--- src/main/java/nl/ekholabs/escode/action/SelectFileAction.java
+++ src/main/java/nl/ekholabs/escode/action/SelectFileAction.java
@@ -1,23 +1,12 @@
 package nl.ekholabs.escode.action;
 
-import nl.ekholabs.escode.core.EsCodeGenerator;
-
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.IOException;
+import java.util.Optional;
 
-import javax.swing.AbstractAction;
-import javax.swing.JComponent;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
-public class SelectFileAction extends AbstractAction {
-
-  private static final long serialVersionUID = -6244737874353836899L;
+public class SelectFileAction {
 
   private final Frame parent;
 
@@ -25,27 +14,15 @@ public class SelectFileAction extends AbstractAction {
     this.parent = parent;
   }
 
-  public void actionPerformed(final ActionEvent event) {
+  public Optional<File> openFile() {
     final JFileChooser chooser = new JFileChooser();
     final int result = chooser.showOpenDialog(parent);
+
     if (result == JFileChooser.APPROVE_OPTION) {
-      final File file = chooser.getSelectedFile();
-
-      try {
-        final EsCodeGenerator esCodeGenerator = new EsCodeGenerator();
-        final JComponent canvas = esCodeGenerator.processImage(file.getAbsolutePath());
-
-        final JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        panel.add(canvas);
-
-        parent.add(panel, BorderLayout.CENTER);
-        parent.validate();
-      } catch (final IOException e) {
-        JOptionPane.showMessageDialog(parent, "Erro occurred when processing the");
-      }
-    } else {
-      JOptionPane.showMessageDialog(parent, "Action was cancelled.");
+      final Optional<File> file = Optional.of(chooser.getSelectedFile());
+      return file;
     }
+
+    return Optional.empty();
   }
 }
